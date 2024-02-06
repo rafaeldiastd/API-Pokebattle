3febe2703ae75e7df19a463660411a3d905344dc
Rafael Dias <rafaeldiastd@gmail.com>
2024-02-06 18:41:18 -0300

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3031;
+const port = 3000;
 const uuid = require("uuid");
 const axios = require("axios");
 const cors = require("cors");
