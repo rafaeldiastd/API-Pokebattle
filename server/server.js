8640d5100941712b2975034f850d49b7ee28cbdc
Rafael Dias <rafaeldiastd@gmail.com>
2024-02-06 18:46:38 -0300

    change message

--- server/server.js
+++ server/server.js
@@ -143,7 +143,7 @@ app.post("/usuario/favoritar/:idUsuario", (req, res) => {
     id,
     name
   });
-  res.json({ message: "O pokémon favoritado com sucesso!" });
+  res.json({ message: `O pokémon ${name} foi favoritado com sucesso!` });
 });
 
 
